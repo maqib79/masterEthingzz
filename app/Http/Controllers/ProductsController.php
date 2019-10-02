397e083d08bec318b69d227dbdd5703e9da5bf47
Muhammad Aqib <maqib79@gmail.com>
2019-10-02 15:22:39 +0500

    latest commit

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -16,12 +16,8 @@ class ProductsController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
         
         $products = Product::paginate(15);
-=======
-        $products = Product::paginate(12);
->>>>>>> 18ec2bb4c3db57ad2fbc27761997d8892ecdcda6
         return view('viewproduct',compact('products'));
     }
     /**
@@ -122,16 +118,10 @@ class ProductsController extends Controller
                 'ProductPrice'=> 'required',
                 'ProductInstock'=> 'required',
                 'ProductQuantity'=> 'required',
-<<<<<<< HEAD
                 // 'ProductSKU'=> 'required|unique:products,ProductSKU',
                 'ProductSKU'=> 'required',
                 'ProductMtitle'=> 'nullable',
                 'ProductMDesc'=> 'nullable',
-=======
-                'ProductSKU'=> 'required',
-                'ProductMtitle'=> 'required',
-                'ProductMDesc'=> '',
->>>>>>> 18ec2bb4c3db57ad2fbc27761997d8892ecdcda6
                 'ProductBrand'=> 'required',
                 'ProductWarranty'=> 'required',
                 'ProductDesc'=> 'required',
